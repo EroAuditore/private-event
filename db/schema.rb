83615bb6b3c28a8143d9be522603893fd7d60bd2
Chukwuma Obasi <ccobasi8@gmail.com>
2021-04-17 11:45:16 +0100

    Add creator id to event table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_204025) do
+ActiveRecord::Schema.define(version: 2021_04_17_103212) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_04_14_204025) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
